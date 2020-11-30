4f2924e6fea130a9a0922b64a94da36d5e28619e
owenasnis <oasnis@college.harvard.edu>
2020-11-29 21:36:49 -0500

    A lot of new work

--- app.R
+++ app.R
@@ -1,111 +1,189 @@
 library(shiny)
 library(tidyverse)
-library(ggthemes)
+library(tidycensus)
+library(usmap)
+library(rstanarm)
+library(gtsummary)
 
-presidents <- read_csv("raw_data/1976-2016-president.csv", 
-                       col_type = cols(year = col_double(),
-                                       state = col_character(),
-                                       state_po = col_character(),
-                                       state_fips = col_double(),
-                                       state_cen = col_double(),
-                                       state_ic = col_double(),
-                                       office = col_character(),
-                                       candidate = col_character(),
-                                       party = col_character(),
-                                       writein = col_logical(),
-                                       candidatevotes = col_double(),
-                                       totalvotes = col_double(),
-                                       version = col_double(),
-                                       notes = col_logical())) %>% 
-    filter(party %in% c("republican", "democrat"), 
-           state %in% c("Arizona", "Florida", "Michigan", 
-                        "North Carolina", "Pennsylvania", "Wisconsin")) %>% 
-    drop_na(candidate) %>% 
-    mutate(candidate_vote_percentage = (candidatevotes / totalvotes) * 100) %>%
-    select(year, state, party, candidate_vote_percentage)
+nhgis <- read_csv("raw_data/nhgis.csv") %>% 
+    mutate(FIPS = paste0(STATEA, COUNTYA), 
+           white_pct = (AF2ME002 / AF2ME001) * 100,
+           raw_white = AF2ME002, 
+           black_pct = (AF2ME003 / AF2ME001) * 100, 
+           raw_black = AF2ME003, 
+           asian_pct = (AF2ME005 / AF2ME001) * 100,
+           raw_asian = AF2ME005, 
+           other_pct = (AF2ME009 / AF2ME001) * 100,
+           raw_other = AF2ME009, 
+           high_school_degree = ((AF4OE017 + AF4OE018) / AF4OE001) * 100,
+           raw_high_school_degree = AF4OE017 + AF4OE018, 
+           college_no_degree = ((AF4OE019 + AF4OE020) / AF4OE001) * 100,
+           raw_college_no_degree = AF4OE019 + AF4OE020, 
+           bachelors_degree = (AF4OE022 / AF4OE001) * 100, 
+           raw_bachelors_degree = AF4OE022, 
+           grad_degree = ((AF4OE023 + AF4OE024 + AF4OE023) / AF4OE001) * 100,
+           raw_grad_degree = AF4OE023 + AF4OE024 + AF4OE023,  
+           median_household_income = AF49E001) %>% 
+    select(FIPS, white_pct, black_pct, asian_pct, other_pct, high_school_degree, 
+           college_no_degree, bachelors_degree, grad_degree, 
+           median_household_income, raw_white, raw_black, raw_asian, raw_other, 
+           raw_high_school_degree, raw_college_no_degree, raw_bachelors_degree, 
+           raw_grad_degree)
 
-county <- read_csv("raw_data/countypres_2000-2016.csv", 
-                   col_type = cols(year = col_double(),
-                                   state = col_character(),
-                                   state_po = col_character(),
-                                   county = col_character(),
-                                   FIPS = col_double(),
-                                   office = col_character(),
-                                   candidate = col_character(),
-                                   party = col_character(),
-                                   candidatevotes = col_double(),
-                                   totalvotes = col_double(),
-                                   version = col_double())) 
+nhgis_pop <- read_csv("raw_data/pop.csv") %>%  
+    mutate(FIPS = paste0(STATEA, COUNTYA), 
+           pop = AF2LE001) %>% 
+    select(FIPS, pop)
 
-# Define UI for application that draws a histogram
+countypres <- read_csv("raw_data/countypres_2000-2016.csv", 
+                       col_types = cols(year = col_double(),
+                                        state = col_character(),
+                                        state_po = col_character(),
+                                        county = col_character(),
+                                        FIPS = col_character(),
+                                        office = col_character(),
+                                        candidate = col_character(),
+                                        party = col_character(),
+                                        candidatevotes = col_double(),
+                                        totalvotes = col_double(),
+                                        version = col_double())) %>%
+    filter(state_po %in% c("PA", "WI", "MI"), 
+           party %in% c("democrat", "republican"), 
+           year %in% c(2012, 2016)) %>% 
+    select(- state, - office, - version) %>% 
+    mutate(vote_share = (candidatevotes/totalvotes) * 100, 
+           party = case_when(party == "democrat" ~ "D", 
+                             party == "republican" ~ "R")) %>%
+    pivot_wider(names_from = "candidate", 
+                values_from = "vote_share") %>% 
+    group_by(FIPS, year) %>% 
+    summarize(Obama = mean(`Barack Obama`, na.rm = TRUE), 
+              Clinton = mean(`Hillary Clinton`, na.rm = TRUE),
+              Romney = mean(`Mitt Romney`, na.rm = TRUE), 
+              Trump = mean(`Donald Trump`, na.rm = TRUE),
+              .groups = "keep") %>% 
+    mutate(dem_vs = if_else(year == 2012, Obama, Clinton), 
+           rep_vs = if_else(year == 2012, Romney, Trump)) %>% 
+    select(- Obama, - Clinton, - Romney, - Trump) %>% 
+    left_join(nhgis, by = "FIPS") %>% 
+    left_join(nhgis_pop, by = "FIPS")
+
+e2012 <- countypres %>% 
+    filter(year == 2012) %>% 
+    ungroup() %>% 
+    select(FIPS, "dem_vs") %>% 
+    rename(fips = FIPS, value = dem_vs)
+
+e2016 <- countypres %>% 
+    filter(year == 2016) %>% 
+    ungroup() %>% 
+    select(FIPS, "dem_vs") %>%
+    rename(fips = FIPS, value = dem_vs)
+
+dem_trend <- countypres %>%
+    mutate(dem_diff = dem_vs - rep_vs) %>%  
+    select(FIPS, year, dem_diff) %>% 
+    group_by(FIPS) %>% 
+    summarize(dem_chg = diff(dem_diff), 
+              .groups = "drop") 
+
+swings <- dem_trend %>% 
+    arrange(dem_chg)
+
+swings_map <- swings %>% 
+    select(FIPS, dem_chg) %>% 
+    rename(fips = FIPS, value = dem_chg)
+
 ui <- navbarPage(
-    "2020 Battleground States: Where the Election Will Be Won and Lost",
-    tabPanel("Recent Battleground Electoral History",
+    "The Blue Wall: Wisconsin, Michigan and Pennslyvania in 2012 and 2016",  
+    tabPanel("Recent Results",
              fluidPage(
-                 titlePanel("Voting Trends in Battleground States"),
-                     mainPanel(plotOutput("Battleground")))),
-    tabPanel("Pennsylvania Battleground County Trends",
-             fluidPage(
-                 titlePanel("Trends in PA Battleground Counties"),
-                 mainPanel(plotOutput("PA")))),
+                 titlePanel("Election Results by County: President"),
+                 mainPanel(plotOutput("Results12"), 
+                           plotOutput("Results16"), 
+                           plotOutput("Swingmap")))),
     tabPanel("About", 
              titlePanel("About"),
-             h3("Project Background and Motivations"),
-             p("My project focuses on presidential election analysis by studying 
-               the 2020 swing states. Specifically, I will study demographic 
-               trends within the swing states. By looking at history 
-               and discovering trends, we can make a prediction for how the 
-               states will vote in the upcoming 2020 election. For my first 
-               graph, I used the U.S. President 1976-2016 data set from the MIT 
-               Election Data + Science Lab. I wrangled the data and created a
-               column that contains the percent vote share of the two major 
-               parties. I selected six swing states from the upcoming election 
-               and visualized the percentage vote share for each party over the 
-               past thirty years. Here is my repository: 
-               https://github.com/owenasnis/Final-Project- . This repository
-               contains the Rmd where I wrangled my data."),
+             h3("Background and Motivations"),
+             p("In 2012, Barack Obama won a second term as the 44th President 
+               of the United States by claiming the battleground states of 
+               Florida, Ohio, and Iowa. Four years later, Donald Trump defeated
+               Hillary Clinton in Florida, Ohio and Iowa. However, even with 
+               these critical battleground victories, Trump still falls 10 
+               electoral votes short. Wisconsin, Michigan and Pennslyvania, 
+               also known as the Blue Wall, stood strong in 2012, but crumbled 
+               in 2016, giving Trump the White House. Therefore, in 
+               contemporary United States Presidential Elections, Wisconsin, 
+               Michigan and Pennsylvania represent the median states. In other 
+               words, the candidate who performs best in Wisconsin, Michigan and 
+               Pennslyvania, wins the electoral college. Just a few weeks ago in 
+               the 2020 election, Joe Biden defeated Donald Trump in all three 
+               states and was elected the 46th President of the United States. 
+               However, data is not completely availible yet for the 2020 
+               election. Soon, a similar analysis can be done for the 2016 and 
+               2020 elections, but for now, this analysis focuses on the 2012
+               and 2016 elections. Given the importance of these three major 
+               battlegrounds, it's important to ask: Why did these states vote 
+               the way they did and which demographic categories are the 
+               strongest predictors? I try my best to answer this question in my
+               project."),
              h3("About Me"),
-             p("My name is Owen Asnis and I study Government at Harvard.  
-             You can reach me at oasnis@college.harvard.edu or 
-               owenasnis@gmail.com.")))
+             p("My name is Owen Asnis and I am an A.B. candidate in Government 
+               on the Public Policy track at Harvard College (class of 2023). 
+               You can reach me at owenasnis@gmail.com or 
+               oasnis@college.harvard.edu.")))
+
 
 server <- function(input, output) {
-    output$Battleground <- renderPlot({
-        presidents %>% 
-            ggplot(aes(x = year, y = candidate_vote_percentage, 
-                       color = party)) + 
-            geom_line() + 
-            facet_wrap(~ state) + 
-            scale_color_manual(labels = c("Democrat", "Republican"), 
-                               values = c("blue2", "red2"), 
-                               name = "Candidate Party", 
-                               breaks = c("democrat", "republican")) + 
-            theme_bw() + 
-            labs(title = "Recent Electoral History of 2020 Swing States", 
-                 subtitle = "How will states trend in 2020?", 
-                 x = "Election Year (1976 - 2016)", 
-                 y = "Percentage Party Vote Share")
+    output$Results12 <- renderPlot({
+        plot_usmap(include = c("WI", "MI", "PA"), 
+                   regions = "counties", 
+                   data = e2012, 
+                   values = "value") + 
+            scale_fill_gradient2(name = "Vote Share", 
+                                 low = "red1",
+                                 mid = "white",
+                                 high = "darkblue", 
+                                 midpoint = 50, 
+                                 breaks = c(25, 50, 75), 
+                                 labels = c("+25% R", "E", "+25% D")) + 
+            labs(title = "2012 Presidential Election Results By County", 
+                 subtitle = "Barack Obama wins all 46 electoral votes", 
+                 caption = "Source: MIT Election Lab") 
     })
-    output$PA <- renderPlot({
-        county %>% 
-            filter(state_po == "PA", 
-                   county %in% c("Erie", "Chester", "Berks", 
-                                 "Lackawanna", "Bucks", "Northampton"), 
-                   party %in% c("democrat", "republican")) %>% 
-            select(- state, - state_po, - FIPS, - office, - version) %>% 
-            mutate(vote_share = candidatevotes/totalvotes) %>% 
-            ggplot(aes(x = year, y = vote_share, color = party)) + 
-            geom_line() + 
-            facet_wrap(~ county) + 
-            scale_y_continuous(labels = scales::percent_format()) + 
-            theme_fivethirtyeight() + 
-            scale_color_manual(labels = c("Democrat", "Republican"), 
-                               values = c("blue2", "red2"), 
-                               name = "Candidate Party", 
-                               breaks = c("democrat", "republican")) + 
-            labs(title = "Recent Voting History in Pennsylvania Swing Counties", 
-                 subtitle = "Republicans Improving in Most Pennsylvania Swing Counties",
-                 x = "Election Year", y = "Candidate Vote Share")
-})}
- 
+    output$Results16 <- renderPlot({
+        plot_usmap(include = c("WI", "MI", "PA"), 
+                   regions = "counties", 
+                   data = e2016, 
+                   values = "value") + 
+            scale_fill_gradient2(name = "Vote Share", 
+                                 low = "red1",
+                                 mid = "white",
+                                 high = "darkblue", 
+                                 midpoint = 50, 
+                                 breaks = c(25, 50, 75), 
+                                 labels = c("+25% R", "E", "+25% D")) + 
+            labs(title = "2016 Presidential Election Results By County", 
+                 subtitle = "Donald Trump wins all 46 electoral votes", 
+                 caption = "Source: MIT Election Lab") 
+    })
+    output$Swingmap <- renderPlot({
+        plot_usmap(include = c("WI", "MI", "PA"), 
+                   regions = "counties", 
+                   data = swings_map, 
+                   values = "value") + 
+            scale_fill_gradient2(name = "Swing", 
+                                 low = "red1",
+                                 mid = "white",
+                                 high = "darkblue", 
+                                 midpoint = 0, 
+                                 breaks = c(10, 0, -10, -20, -30), 
+                                 labels = c("+10% D", "Even", "+10% R", 
+                                            "+20% R", "+30% R")) + 
+            labs(title = "2016 Change From 2012", 
+                 subtitle = "Republicans gain in rural areas, while Democrats fail to grow lead in urban centers", 
+                 caption = "Source: MIT Election Lab")
+    })
+}
+
 shinyApp(ui = ui, server = server)
