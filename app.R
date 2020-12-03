f2cc809f97a125b914d0cc363e4b8d596217e9b6
owenasnis <oasnis@college.harvard.edu>
2020-12-03 02:26:32 -0500

    New updates... alot

--- app.R
+++ app.R
@@ -74,67 +74,6 @@ countypres <- read_csv("raw_data/countypres_2000-2016.csv",
     left_join(nhgis, by = "FIPS") %>% 
     left_join(nhgis_pop, by = "FIPS")
 
-dem_trend <- countypres %>%
-    mutate(dem_diff = dem_vs - rep_vs) %>%  
-    select(FIPS, year, dem_diff) %>% 
-    group_by(FIPS) %>% 
-    summarize(dem_chg = diff(dem_diff), 
-              .groups = "drop") 
-
-swings <- dem_trend %>% 
-    arrange(dem_chg)
-
-swings_map_r <- swings %>% 
-    select(FIPS, dem_chg) %>% 
-    filter(dem_chg < 0) %>% 
-    rename(fips = FIPS, value = dem_chg)
-
-swings_map_d <- swings %>% 
-    select(FIPS, dem_chg) %>% 
-    filter(dem_chg > 0) %>% 
-    rename(fips = FIPS, value = dem_chg)
-
-demographics <- countypres %>% 
-    ungroup() %>% 
-    filter(year == 2016) %>% 
-    select(-year, -dem_vs, -rep_vs) 
-
-top_25_summary_stats <- swings %>% 
-    arrange(desc(abs(dem_chg))) %>% 
-    head(25) %>% 
-    left_join(demographics, by = "FIPS") %>% 
-    summarize(total_white = sum(raw_white), 
-              total_nonwhite = sum(raw_black + raw_asian + raw_other), 
-              total_less_college = sum(raw_high_school_degree + 
-                                           raw_college_no_degree), 
-              total_college_more = sum(raw_bachelors_degree + raw_grad_degree), 
-              total_population = sum(pop), 
-              avg_pop = mean(pop), 
-              avg_income = mean(median_household_income)) %>% 
-    mutate(white_pct = (total_white / total_population) * 100, 
-           nonwhite_pct = (total_nonwhite / total_population) * 100, 
-           less_college_pct = (total_less_college / total_population) * 100, 
-           college_more_pct = (total_college_more / total_population) * 100) %>%  
-    select(white_pct, nonwhite_pct, less_college_pct, college_more_pct, avg_pop, 
-           avg_income)
-
-summary_stats <- swings %>% 
-    left_join(demographics, by = "FIPS") %>% 
-    summarize(total_white = sum(raw_white), 
-              total_nonwhite = sum(raw_black + raw_asian + raw_other), 
-              total_less_college = sum(raw_high_school_degree + 
-                                           raw_college_no_degree), 
-              total_college_more = sum(raw_bachelors_degree + raw_grad_degree), 
-              total_population = sum(pop), 
-              avg_pop = mean(pop),
-              avg_income = mean(median_household_income)) %>% 
-    mutate(white_pct = (total_white / total_population) * 100, 
-           nonwhite_pct = (total_nonwhite / total_population) * 100, 
-           less_college_pct = (total_less_college / total_population) * 100, 
-           college_more_pct = (total_college_more / total_population) * 100) %>%  
-    select(white_pct, nonwhite_pct, less_college_pct, college_more_pct, avg_pop, 
-           avg_income)
-
 countypres12 <- countypres %>% 
     filter(year == 2012) %>% 
     mutate(nonwhite_pct = black_pct + asian_pct + other_pct, 
@@ -156,45 +95,77 @@ rustbelt_geometry <- get_acs(geography = "county",
 
 map12 <- countypres12 %>%
     ungroup() %>% 
-    select(FIPS, dem_vs, rep_vs) %>% 
+    select(FIPS, dem_vs, rep_vs) %>%
+    left_join(nhgis_pop, by = "FIPS") %>% 
     rename(fips = FIPS) %>% 
     mutate(difference = dem_vs - rep_vs) %>% 
     left_join(rustbelt_geometry, by = "fips") %>% 
     mutate(dem_vs = round(dem_vs, digits = 1), 
-           rep_vs = round(rep_vs, digits = 1))
+           rep_vs = round(rep_vs, digits = 1), 
+           state = case_when(str_sub(fips, 1, 2) == 26 ~ "Michigan", 
+                             str_sub(fips, 1, 2) == 42 ~ "Pennsylvania", 
+                             str_sub(fips, 1, 2) == 55 ~ "Wisconsin")) %>%   
+    group_by(state) %>% 
+    mutate(pop_rank = rank(desc(pop)), 
+           total_counties = n())
 
 map16 <- countypres16 %>% 
     ungroup() %>% 
-    select(FIPS, dem_vs, rep_vs) %>% 
+    select(FIPS, dem_vs, rep_vs) %>%
+    left_join(nhgis_pop, by = "FIPS") %>% 
     rename(fips = FIPS) %>% 
     mutate(difference = dem_vs - rep_vs) %>% 
     left_join(rustbelt_geometry, by = "fips") %>% 
     mutate(dem_vs = round(dem_vs, digits = 1), 
-           rep_vs = round(rep_vs, digits = 1))
+           rep_vs = round(rep_vs, digits = 1), 
+           state = case_when(str_sub(fips, 1, 2) == 26 ~ "Michigan", 
+                             str_sub(fips, 1, 2) == 42 ~ "Pennsylvania", 
+                             str_sub(fips, 1, 2) == 55 ~ "Wisconsin")) %>%   
+    group_by(state) %>% 
+    mutate(pop_rank = rank(desc(pop)), 
+           total_counties = n())
+
+trend <- countypres %>%
+    mutate(dem_diff = dem_vs - rep_vs) %>%  
+    select(FIPS, year, dem_diff) %>%
+    group_by(FIPS) %>% 
+    summarize(dem_chg = diff(dem_diff), 
+              .groups = "drop") %>%
+    left_join(nhgis_pop, by = "FIPS") %>%
+    rename(fips = FIPS) %>%
+    left_join(rustbelt_geometry, by = "fips") %>% 
+    mutate(dem_chg = round(dem_chg, digits = 1)) %>% 
+    mutate(state = case_when(str_sub(fips, 1, 2) == 26 ~ "Michigan", 
+                             str_sub(fips, 1, 2) == 42 ~ "Pennsylvania", 
+                             str_sub(fips, 1, 2) == 55 ~ "Wisconsin")) %>%   
+    group_by(state) %>% 
+    mutate(pop_rank = rank(desc(pop)), 
+           total_counties = n())
 
 ui <- navbarPage(
-    "The Blue Wall: How Wisconsin, Michigan and Pennsylvania Decide Elections",
+    "The Battleground: Wisconsin, Michigan and Pennsylvania in Contemporary Presidential Elections",
     theme = shinytheme("sandstone"), 
     tabPanel("About the Project",
-             h3("The State of Wisconsin (10 Electoral Votes)"),
+             fluidPage(column(4,
+             h3("Wisconsin (10 Electoral Votes)"),
              p("In 2012, President Barack Obama defeated Mitt Romney in 
                Wisconsin by 205,204 votes (roughly 6.7%), claiming the state's 
                10 electoral votes. However, four years later, Donald Trump 
                defeated Hillary Clinton in Wisconsin by 22,748 votes(roughly 
                0.7%). In four years, Wisconsin's electorate swung by 227,952 
                votes."),
-             h3("The State of Michigan (16 Electoral Votes)"), 
+             h3("Michigan (16 Electoral Votes)"), 
              p("In 2012, President Obama defeated Romney in Michigan by 449,238 
                votes (roughly 9.5%), claiming the state's 16 electoral votes. 
                Four years later, Trump defeated Clinton in Michigan by 10,704 
                votes (roughly 0.3%). In four years, Michigan's electorate swung 
-               by 459,942 votes."),
-             h3("The Commonwealth of Pennsylvania (20 Electoral Votes)"), 
+               by 459,942 votes."), 
+             h3("Pennsylvania (20 Electoral Votes)"), 
              p("In 2012, President Obama defeated Romney in Pennsylvania by 
                287,865 votes (roughly 5.2%). claiming the commonwealth's 20 
                electoral votes. Four years later, Trump defeated Clinton in 
                Pennsylvania by 44,292 votes (roughly 0.7%). In four years, 
-               Pennsylvania's electorate swung by 332,157 votes."), 
+               Pennsylvania's electorate swung by 332,157 votes.")), 
              h3("The Project: Motivation and Summary"), 
              p("Wisconsin, Michigan and Pennsylvania have 46 votes in the 
                electoral college, and since 1988, these states have voted for 
@@ -220,122 +191,118 @@ ui <- navbarPage(
              p("My name is Owen Asnis and I am an A.B. candidate in Government 
                on the Public Policy track at Harvard College (class of 2023). 
                You can reach me at owenasnis@gmail.com or 
-               oasnis@college.harvard.edu.")),  
+               oasnis@college.harvard.edu."))),  
     tabPanel("Results: President", 
              fluidPage(
-                 titlePanel("Recent Results: President"), 
+                 titlePanel("Results: President"), 
                  sidebarLayout(
                      sidebarPanel(
-                         selectInput("select",
+                         selectInput("year",
                                      "Election Year:", 
                                      choices = list("2012", "2016"))),
                      mainPanel(
-                         plotlyOutput("results", width = "100%"))))), 
+                         plotlyOutput("results"))))), 
     tabPanel("What Changed?", 
              fluidPage(
-                 titlePanel("2012 versus 2016: Swing Counties"), 
-                 mainPanel(plotOutput("trend"), 
-                           plotOutput("rpickup"), 
-                           plotOutput("dpickup")))), 
-    tabPanel("Models and Stats", 
+                 titlePanel("What Changed?"), 
+                 mainPanel(plotlyOutput("swingsmap")),
+                 fluidPage(column(12, 
+                 sidebarLayout(
+                     sidebarPanel(
+                         selectInput("state",
+                                     "Select a state:", 
+                                     choices = list("Wisconsin",
+                                                    "Michigan", 
+                                                    "Pennsylvania"))),
+                     mainPanel(
+                         plotOutput("countychange"))))))), 
+    tabPanel("Models", 
              fluidPage(
                  titlePanel("Model: Major Demographics and Vote Share"), 
                  mainPanel(tableOutput("compare")))))
 
 server <- function(input, output) {
     output$results <- renderPlotly({
-        if(input$select == "2012"){
+        if(input$year == "2012"){
             ggplotly(ggplot(data = map12, 
                             aes(geometry = geometry, 
-                                text = paste(NAME, "<br>",
-                                             "Barack H. Obama (D):", dem_vs, "%", "<br>", 
-                                             "Mitt Romney (R):", rep_vs, "%"))) +
-                         geom_sf(aes(fill = difference < 0),
+                                text = paste("2012 RESULTS: PRESIDENT", "<br>", 
+                                             NAME, "<br>",
+                                             "Obama/Biden (D - inc):", dem_vs, "%", "<br>", 
+                                             "Romney/Ryan (R):", rep_vs, "%", "<br>", 
+                                             "State Population Rank:", pop_rank, "of", total_counties))) +
+                         geom_sf(aes(fill = difference),
                                  show.legend = FALSE) + 
                          theme_map() + 
-                         scale_fill_manual(values = c("deepskyblue", "red1")),
+                         scale_fill_gradient2(name = "Vote Share", 
+                                              low = "red2",
+                                              mid = "white",
+                                              high = "blue3", 
+                                              midpoint = 0) + 
+                         labs(title = "2012 Results By County (hover for results)"), 
                      tooltip = c("text")) %>%
-                layout(showlegend = FALSE) 
+                layout(showlegend = FALSE)  
         }
         else if(input$select == "2016"){
             ggplotly(ggplot(data = map16, 
                             aes(geometry = geometry, 
-                                text = paste(NAME, "<br>",
-                                             "Hillary R. Clinton (D):", dem_vs, "%", "<br>", 
-                                             "Donald J. Trump (R):", rep_vs, "%"))) +
-                         geom_sf(aes(fill = difference < 0),
+                                text = paste("2016 RESULTS: PRESIDENT", "<br>", 
+                                NAME, "<br>",
+                                "Clinton/Kaine (D):", dem_vs, "%", "<br>", 
+                                "Trump/Pence (R):", rep_vs, "%", "<br>", 
+                                "State Population Rank:", pop_rank, "of", total_counties)))+
+                         geom_sf(aes(fill = difference),
                                  show.legend = FALSE) + 
                          theme_map() + 
-                         scale_fill_manual(values = c("deepskyblue", "red1")),
+                         scale_fill_gradient2(name = "Vote Share", 
+                                              low = "red2",
+                                              mid = "white",
+                                              high = "blue3", 
+                                              midpoint = 0) + 
+                         labs(title = "2016 Results By County (hover for results)"), 
                      tooltip = c("text")) %>%
                 layout(showlegend = FALSE)  
         }
     })
-    output$trend <- renderPlot({
-        dem_trend %>% 
-            mutate(state = case_when(str_sub(FIPS, 1, 2) == 26 ~ "Michigan", 
-                                     str_sub(FIPS, 1, 2) == 42 ~ "Pennsylvania", 
-                                     str_sub(FIPS, 1, 2) == 55 ~ "Wisconsin")) %>%
-            ggplot(aes(x = dem_chg, fill = state)) + 
-            geom_density(alpha = 0.75) + 
+    output$swingsmap <- renderPlotly({
+        ggplotly(ggplot(data = trend, aes(geometry = geometry, 
+                                          text = paste("COUNTY SWINGS", "<br>", 
+                                                       NAME, "<br>", 
+                                                       "Swing:", "+", abs(dem_chg), "%", 
+                                                       case_when(dem_chg > 0 ~ "towards Democrats", 
+                                                                 dem_chg < 0 ~ "towards Republicans", 
+                                                                 dem_chg == 0 ~ "EVEN"), "<br>", 
+                                                       "State Population Rank:", pop_rank, "of", total_counties))) + 
+                     geom_sf(aes(fill = dem_chg), 
+                             show.legend = FALSE) + 
+                     theme_map() +
+                     labs(title = "County Swings (hover for swings)") + 
+                     scale_fill_gradient2( low = "red2",
+                                          mid = "white",
+                                          high = "blue3", 
+                                          midpoint = 0), 
+                 tooltip = c("text")) %>% 
+            layout(showlegend = FALSE)
+    })
+    output$countychange <- renderPlot({
+        trend %>% 
+            filter(state == input$state) %>% 
+        ggplot(aes(x = dem_chg)) + 
+            geom_histogram(binwidth = 5, 
+                           color = "white", 
+                           fill = "dodgerblue2") + 
             scale_x_continuous(breaks = c(-30, -20, -10, 0, 10), 
                                labels = c("+30% R", "+20% R", "+10% R", 
-                                          "Even", "+10% D")) + 
-            scale_fill_manual(breaks = c("Michigan", "Pennsylvania", "Wisconsin"), 
-                              values = c("darkgreen", "midnightblue", "red3"), 
-                              name = "County State") +
-            labs(title = "Vote Share Change from 2012 to 2016", 
-                 subtitle = "Republicans gain in nearly all MI, PA and WI counties", 
+                                          "Even", "+10% D")) +
+            labs(title = "County Swings in Selected State", 
                  x = "Vote Share Change", 
-                 y = "Percentage of Counties") + 
-            theme_minimal() + 
-            scale_y_continuous(labels = scales::percent_format())
-    })
-    output$rpickup <- renderPlot({
-        plot_usmap(include = c("WI", "MI", "PA"), 
-                   regions = "counties", 
-                   data = swings_map_r, 
-                   values = "value") + 
-            scale_fill_gradient2(name = "Swing", 
-                                 low = "red1",
-                                 mid = "white",
-                                 high = "darkblue", 
-                                 midpoint = 0, 
-                                 breaks = c(0, -10, -20, -30), 
-                                 labels = c("Even", "+10% R", "+20% R", "+30% R")) + 
-            labs(title = "Counties Where Republicans Outperformed 2012", 
-                 subtitle = "Huge rural gains, and minor gains in urban centers", 
-                 caption = "Source: MIT Election Lab")
-    })
-    output$dpickup <- renderPlot({
-        plot_usmap(include = c("WI", "MI", "PA"), 
-                   regions = "counties", 
-                   data = swings_map_d, 
-                   values = "value") + 
-            scale_fill_gradient2(name = "Swing", 
-                                 low = "red1",
-                                 mid = "white",
-                                 high = "darkblue", 
-                                 midpoint = 0, 
-                                 breaks = c(0, 10), 
-                                 labels = c("Even", "+10% D")) + 
-            labs(title = "Counties Where Democrats Outperformed 2012", 
-                 subtitle = "Minor suburban gains, but failure in rural areas and urban centers", 
-                 caption = "Source: MIT Election Lab")
-    })
-    output$compare <- renderTable({
-        full_join(top_25_summary_stats, summary_stats, 
-                  by = c("white_pct", "nonwhite_pct", 
-                         "less_college_pct", "college_more_pct", 
-                         "avg_pop", "avg_income")) %>% 
-            mutate(Source = c("Top 25 Swing Counties", "All Counties")) %>% 
-            rename("White %" = white_pct, 
-                   "Non-white %" = nonwhite_pct, 
-                   "No College Degree %" = less_college_pct, 
-                   "At Least College Degree %" = college_more_pct, 
-                   "Average Population" = avg_pop, 
-                   "Average Median Household Income" = avg_income) %>% 
-            relocate("Source", .before = "White %")
+                 y = "Number of Couties Selected State", 
+                 subtitle = "Most counties swing towards Republicans") + 
+            theme_clean() +  
+            geom_vline(xintercept = 0, 
+                       alpha = 0.5, 
+                       color = "purple", 
+                       )
     })
 }
 
